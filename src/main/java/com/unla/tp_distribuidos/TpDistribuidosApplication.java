480a1705652dae487ff6a655d4895d932b9abfaa
AgustinCorno <agustincorno@gmail.com>
2026-09-10 17:36:53 -0300

    Implementacion completa Hito 1 (REST, GraphQL, Swagger, Web UI) e Hito 2 (gRPC)

---
+++ src/main/java/com/unla/tp_distribuidos/TpDistribuidosApplication.java
@@ -0,0 +1,13 @@
+package com.unla.tp_distribuidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpDistribuidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpDistribuidosApplication.class, args);
+	}
+
+}
